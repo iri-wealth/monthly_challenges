859d84f0c6b613a9f4bea0531a7c06df15f20159
iri_wealth <116193529+iri-wealth@users.noreply.github.com>
2023-05-10 13:14:09 +0200

    Add files via upload

--- monthly_challenges/settings.py
+++ monthly_challenges/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR / 'challenges' / 'templates'
-            BASE_DIR /"monthly_challenges"/"templates"
+            BASE_DIR /"templates"
             ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR /'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
